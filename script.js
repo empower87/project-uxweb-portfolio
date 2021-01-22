4f68d472a829e1196ee0f03aae729502f6215991
empower87 <mjamespower87@gmail.com>
2021-01-22 11:17:17 -0800

    new commit

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-document.getElementById("depop-button").onclick = function () {
-  location.href = "www.depop.com";
-};
+let nav = document.querySelector('nav')
+console.log(nav.style.background.valueOf)
+
+document.querySelector('#menuToggle input').oninput = function() {
+  if (nav == "rgba(0, 0, 0, 0)") {
+    nav.style.background = "#00000096"
+    console.log(nav)
+  }
+}
