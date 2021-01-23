97ff130dad61a74cb6f73e81baa3ce089ee539bc
empower87 <mjamespower87@gmail.com>
2021-01-22 19:51:37 -0800

    4 outta 6 done

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 let nav = document.querySelector('nav')
-console.log(nav.style.background.valueOf)
+console.log(nav.style.background)
 
-document.querySelector('#menuToggle input').oninput = function() {
+document.querySelector('#menuToggle input').oninput = function () {
   if (nav == "rgba(0, 0, 0, 0)") {
     nav.style.background = "#00000096"
     console.log(nav)
   }
 }
+
+let previousY;
+let page = 0; 
+document.querySelectorAll('.slide-wrap a').forEach(link => {
+
+  link.onclick = function (e) {
+
+    document.querySelectorAll('.slide-wrap a').forEach(link => {
+      link.classList.remove('page')
+    })
+
+    previousY = window.scrollY
+  
+    this.classList.add('page')
+
+  setTimeout(function(){
+    window.scrollTo(0, previousY)
+  }, 0)
+  }
+})
+
+console.log("page loaded")
